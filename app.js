e8fb1bf23a3e2b6c78236e857d6e9f39c4bcdb10
unknown <nitishonspeed@gmail.com>
2016-08-30 10:23:15 +0530

    Able to tweet via node

--- app.js
+++ app.js
@@ -2,14 +2,34 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var authenticationHandler=require('./routes/auth.js');
+var Twit = require('twit');
+
+
+app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/',authenticationHandler);
+
+
+var config=require('./configuration/config.js');
+var T = new Twit(config);
+
+//
+//  tweet 'hello world!'
+//
+var tweet={ status: 'Good evening pals RT for free follow' };
+T.post('statuses/update', tweet, function(err, data, response) {
+  if(err){
+  	console.log("Something went wrong: ",err)
+  }else{
+  	console.log("Worked! ",data.text)
+  }
+});	
 //  app.post('/', function (req, res) {
 //  	console.log("req." ,req.body.user)
 //   res.send('Hello World!');
 // });
-app.use(express.static('public'));
+
 
 //app.use(express.static('files'));
 
---
+++ configuration/config.js
@@ -0,0 +1,6 @@
+module.exports={
+	consumer_key:         'QXeMRsHOso4yA45N0ekPpzpYR',
+  consumer_secret:      'K3YtbavDF6IfBXd01w9cBwBKSmFxodd1RDfxShHXSaJ6hYv8St',
+  access_token:         '1665562615-zd497Rcc1vePlWGfxIUaIbxBZ8XeOaDR1Bm0ncO',
+  access_token_secret:  'eQnAGEpL9ZzawtXxoCZbGDHCZyr9Cs9NNBjdGMHRvjkNp',
+}
